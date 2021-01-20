47175a9f2bbb2c255608327157911d1016478ff3
quicksilversel <quicksilversel@gmail.com>
2021-01-20 21:28:22 +0900

    fixed unit error

--- unitconverter/Util.swift
+++ unitconverter/Util.swift
@@ -22,43 +22,43 @@ func converter(string: String) -> String {
     // temperature
     case "c":
         temp = Float((num! * 9/5) + 32)
-        result = String(format: "%.2f", temp) + " " + "mL"
+        result = String(format: "%.2f", temp) + " " + "F"
     case "f":
         temp = Float((num! - 32) * 5/9)
-        result = String(format: "%.2f", temp) + " " + "mL"
+        result = String(format: "%.2f", temp) + " " + "C"
     // weight
     case "pounds", "lbs":
         temp = Float((num! / 2.205))
-        result = String(format: "%.2f", temp) + " " + "mL"
+        result = String(format: "%.2f", temp) + " " + "kg"
     case "g":
         temp = Float((num! / 28.35))
-        result = String(format: "%.2f", temp) + " " + "mL"
+        result = String(format: "%.2f", temp) + " " + "oz"
     case "kg":
         temp = Float((num! * 2.205))
-        result = String(format: "%.2f", temp) + " " + "mL"
+        result = String(format: "%.2f", temp) + " " + "lbs"
     // length
     case "inches", "inch", "in":
         temp = Float((num! * 2.54))
-        result = String(format: "%.2f", temp) + " " + "mL"
+        result = String(format: "%.2f", temp) + " " + "cm"
     case "feet", "foot", "ft":
         temp = Float((num! / 3.281))
-        result = String(format: "%.2f", temp) + " " + "mL"
+        result = String(format: "%.2f", temp) + " " + "m"
     case "mile", "miles":
         temp = Float((num! * 1.609))
-        result = String(format: "%.2f", temp) + " " + "mL"
+        result = String(format: "%.2f", temp) + " " + "km"
     case "cm":
         temp = Float((num! / 2.54))
-        result = String(format: "%.2f", temp) + " " + "mL"
+        result = String(format: "%.2f", temp) + " " + "inches"
     case "m":
         temp = Float((num! * 3.281))
-        result = String(format: "%.2f", temp) + " " + "mL"
+        result = String(format: "%.2f", temp) + " " + "feet"
     case "km":
         temp = Float((num! / 1.609))
-        result = String(format: "%.2f", temp) + " " + "mL"
+        result = String(format: "%.2f", temp) + " " + "miles"
     // volume
     case "gallon", "gallons":
         temp = Float((num! * 3.785))
-        result = String(format: "%.2f", temp) + " " + "mL"
+        result = String(format: "%.2f", temp) + " " + "L"
     case "cup", "cups":
         temp = Float((num! * 237))
         result = String(format: "%.2f", temp) + " " + "mL"
